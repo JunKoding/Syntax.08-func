d3b276e15cb7705e3521a4d0e47fa4762c686462
JunKoding <wnsrb0407@daum.net>
2022-01-31 18:42:03 +0900

    스위프트 기초 문법- 08. 함수 매개변수 이름

---
+++ 08.playground/Contents.swift
@@ -0,0 +1,30 @@
+import Cocoa
+
+// 스위프트 기초 문법 08 함수 매개변수 이름
+
+// 함수, 메소드 정의
+func myFunction(name: String) -> String {
+    return "안녕하세요. \(name)입니다."
+}
+// 함수, 메소드를 호출한다. call
+print(myFunction(name: "이준규"))
+// 안녕하세요. 이준규입니다.
+
+
+
+// 매개변수 이름을 바꾸고 싶을때
+func myFunction2(with name: String) -> String {
+    return "안녕하세요. \(name)입니다."
+}
+print(myFunction2(with: "수원삼성"))
+// 안녕하세요. 수원삼성입니다.
+
+
+
+// 매개변수 이름도 쓰기 싫을때는 언더바 (_)
+func myFunction3(_ name: String) -> String {
+    return "안녕하세요. \(name)입니다."
+}
+print(myFunction3("수원삼성우승"))
+// 안녕하세요. 수원삼성우승입니다.
+
